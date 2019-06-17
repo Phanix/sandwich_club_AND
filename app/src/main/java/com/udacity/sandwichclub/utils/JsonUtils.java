561b23486b0d4e32074c11f05ec9759ea815b228
fabio henrique da silva <hantarodev@gmailcom>
2019-06-17 15:37:02 -0300

    Initial commit

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,37 @@
+package com.udacity.sandwichclub.utils;
+
+import android.util.Log;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject name = jsonObject.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            JSONArray alsoKnownAs = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAsArray =new ArrayList<>();
+            for(int i = 0; i < alsoKnownAs.length(); i++){
+                alsoKnownAsArray.add(alsoKnownAs.getString(i));
+            }
+            sandwich.setAlsoKnownAs(alsoKnownAsArray);
+            sandwich.setMainName(mainName);
+
+
+
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        return sandwich;
+    }
+}
